c979d859a65a621cd918a64affc95be36bf41291
Babalas <67129025+babalasvr@users.noreply.github.com>
2022-07-12 11:49:24 -0300

    js + pagina cadastro e style2

---
+++ script.js
@@ -0,0 +1,11 @@
+function clicar()
+{
+    var cadastrar = document.querySelector("a.cadastro")
+    cadastrar = window.location.href = "./cadastro.html";
+}
+
+function clicar2()
+{
+    var login = document.querySelector("button.btn")
+    login = window.location.href = "./index.html";
+}
